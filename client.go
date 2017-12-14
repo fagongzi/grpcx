ec45cb8dafa964338d8ad4f12d0128121327cb23
zhangxu19830126@gmail.com <zhangxu19830126@gmail.com>
2017-12-14 16:04:22 +0800

    dev: init

---
+++ client.go
@@ -0,0 +1,80 @@
+package grpcx
+
+import (
+	"fmt"
+	"sync"
+
+	"google.golang.org/grpc"
+)
+
+// ClientCreator create a grpc client
+type ClientCreator func(string, *grpc.ClientConn) interface{}
+
+// GRPCClient is a grpc client
+type GRPCClient struct {
+	sync.RWMutex
+
+	prefix  string
+	creator ClientCreator
+	opts    *clientOptions
+	clients map[string]interface{}
+}
+
+// NewGRPCClient returns a GRPC Client
+func NewGRPCClient(prefix string, creator ClientCreator, opts ...ClientOption) *GRPCClient {
+	copts := &clientOptions{}
+	for _, opt := range opts {
+		opt(copts)
+	}
+
+	return &GRPCClient{
+		prefix:  prefix,
+		opts:    copts,
+		creator: creator,
+		clients: make(map[string]interface{}),
+	}
+}
+
+// GetServiceClient returns a grpc client
+func (c *GRPCClient) GetServiceClient(name string) (interface{}, error) {
+	c.RLock()
+	if cli, ok := c.clients[name]; ok {
+		c.RUnlock()
+		return cli, nil
+	}
+	c.RUnlock()
+
+	client, err := c.createClient(name)
+	if err != nil {
+		c.RUnlock()
+		return nil, err
+	}
+
+	c.RUnlock()
+	return client, nil
+}
+
+func (c *GRPCClient) createClient(name string) (interface{}, error) {
+	c.Lock()
+	defer c.Unlock()
+
+	if cli, ok := c.clients[name]; ok {
+		return cli, nil
+	}
+
+	var grpcOptions []grpc.DialOption
+	grpcOptions = append(grpcOptions, grpc.WithInsecure())
+	grpcOptions = append(grpcOptions, grpc.WithTimeout(c.opts.timeout))
+	grpcOptions = append(grpcOptions, grpc.WithBlock())
+	grpcOptions = append(grpcOptions, grpc.WithBalancer(grpc.RoundRobin(c.opts.resolver)))
+	conn, err := grpc.Dial(fmt.Sprintf("%s/%s", c.prefix, name), grpcOptions...)
+	if err != nil {
+		return nil, err
+	}
+
+	cli := c.creator(name, conn)
+	c.clients[name] = cli
+	c.Unlock()
+
+	return cli, nil
+}
